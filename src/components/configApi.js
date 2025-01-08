1fcf7ce505463fd546cc259835cad33f8cb86fa9
kartikpande123 <kartikpande67@gmail.com>
2025-01-08 18:27:27 +0530

    Fixed API URL to point to the correct domain2

--- src/components/configApi.js
+++ src/components/configApi.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = "http://localhost:5555";
-// const API_BASE_URL = "https://ayanexamadmin.in";
+
+const API_BASE_URL = "https://ayanexamadmin.in";
 export default API_BASE_URL;
+
+// const API_BASE_URL = "http://localhost:5555";
