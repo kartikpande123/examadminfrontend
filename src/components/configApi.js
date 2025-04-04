5b726c3f6d201334f6ce67249f180f7e11ea3476
kartikpande123 <kartikpande67@gmail.com>
2025-04-04 12:08:18 +0530

    Fixed API call

--- src/components/configApi.js
+++ src/components/configApi.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://ayanexamadmin.in";
-const API_BASE_URL = "http://localhost:5555";
+const API_BASE_URL = "https://ayanexamadmin.in";
+// const API_BASE_URL = "http://localhost:5555";
 
 export default API_BASE_URL;
 
